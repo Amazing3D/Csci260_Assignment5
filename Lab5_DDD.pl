150583432a0871a6d94b4d1012188afe2c7e36fe
Amazing3D <dillon.dall@gmail.com>
2017-10-14 16:28:32 -0500

    parts 1 and 2
    These parts are pretty much done. Part 2 may need a little more work but is pretty much functional now (if user enters value not listed (tie or out of range)

--- Lab5_DDD.pl
+++ Lab5_DDD.pl
@@ -8,9 +8,12 @@
 
 use LWP::Simple;
 use strict;
+use utf8;
 
-my ( $webpage, $currentPage, $currentsection, $info );
+my ( $webpage, $currentPage, $currentsection, $info, $selection );
 
+#Ip address and Host Name
+print "\nIp address and Host Name\n";
 $currentPage = "https://www.iplocation.net/find-ip-address";
 
 $webpage = get $currentPage;
@@ -18,4 +21,27 @@ $webpage = get $currentPage;
 $webpage =~ /<table class="iptable">(.*?)<\/table>/s;
 $currentsection = $1;
 
-#print $1;
+$currentsection =~ /((\d{1,3}\.){3}\d{1,3})/;
+print "Your IP is: $1\n";
+
+$currentsection =~ /<th>Host Name<\/th><td>(.*)<\/td>/;
+print "Your Host Name is: $1\n";
+
+print "\n\n";
+
+#North Dakota wikipedia selection
+print "North Dakota wikipedia selection\nSelect an ranking: ";
+$currentPage = "https://en.wikipedia.org/wiki/List_of_cities_in_North_Dakota";
+$webpage     = get $currentPage;
+
+$webpage =~ /<table class="wikitable sortable">(.*?)<\/table>/su;
+$currentsection = $1;
+chomp( $selection = <STDIN> );
+while ( $currentsection =~ /<\/span>$selection(\-\(T\))?<\/td>.*?North Dakota">(.*?)<\/a><\/td>.*?">(.*?)<\/td>/gs ) {
+    if ($1) {
+        print "$selection-Tie : $2 : $3\n";
+    }
+    else {
+        print "$selection : $2 : $3\n";
+    }
+}
